e745b63cf1c5f5db0843769d956c823c8d3ff79e
Лёша Портнягин <101197621+Portlex03@users.noreply.github.com>
2023-06-12 21:07:40 +0600

    Add files via upload

---
+++ 9.02 Прима и Краскал/Main.cs
@@ -0,0 +1,14 @@
+﻿namespace Прима_и_Краскал;
+class Program
+{
+    static void Main()
+    {
+        StreamReader f = new StreamReader(@"C:\\Users\\Azerty\\Desktop\\Ввод.txt");
+        int n = int.Parse(f.ReadLine()) + 1; // кол-во вершин
+        string[] strPeaks = f.ReadLine().Split(); // вершины
+        string[] strValues = f.ReadLine().Split(); // веса
+
+        Prima.Algorithm(strPeaks,strValues, n);
+        Kraskal.Algorithm(strPeaks, strValues, n);
+    }
+}
