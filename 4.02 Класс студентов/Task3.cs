a1ef8d648c465fdcd273266fca047e73af755074
Лёша Портнягин <101197621+Portlex03@users.noreply.github.com>
2023-06-12 21:01:51 +0600

    Add files via upload

---
+++ 4.02 Класс студентов/Task3.cs
@@ -0,0 +1,40 @@
+﻿namespace MySpace;
+
+internal class Task3
+{
+    public static void HonorStudents(in List<StudentInfo> Students)
+    {
+        Console.Clear();
+        StudentInfo.Message("Студенты - отличники");
+        int count = 0;
+        
+        if (StudentInfo.Subjects != null)
+        {
+            Console.WriteLine("\nСписок студентов отличников:");
+            for (int i = 0; i < Students.Count; i++)
+            {
+                int summ = 0;
+                for (int j = 0; j < StudentInfo.Subjects.Length; j++)
+                {
+                    if (Students[i].score[j] == 5)
+                    {
+                        summ ++; 
+                        count++;
+                    }
+                }
+                if (summ == StudentInfo.Subjects.Length)
+                {
+                    Console.WriteLine($"{Students[i].surname} {Students[i].name} {Students[i].group}, средний балл - 5");
+                }
+            }
+            if (count == 0) Console.WriteLine("Не найдено");
+        }
+        else if (Students.Count != 0) Console.WriteLine("\nНет данных, нужно создать список предметов");
+        else Console.WriteLine("\nНет данных, нужно создать список студентов");
+
+        StudentInfo.Message("\nДля выхода в меню нажмите *Enter* ");
+        Console.ReadKey();
+        Console.Clear();
+        Project.Menu(Students);
+    }
+}
---
+++ 4.02 Класс студентов/Task4.cs
@@ -0,0 +1,30 @@
+﻿namespace MySpace;
+
+internal class Task4
+{
+    public static void YoungStudents(in List<StudentInfo> Students)
+    {
+        Console.Clear();
+        StudentInfo.Message("Студенты, которые моложе 20 лет\n");
+
+        if (Students.Count != 0)
+        {
+            int count = 0;
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (2023 - Students[i].date[2] < 20 || 23 - Students[i].date[2] < 20)
+                {
+                    Console.WriteLine($"{Students[i].surname} {Students[i].name} {Students[i].group}");
+                    count++;
+                }
+            }
+            if (count == 0) Console.WriteLine("Не найдено");
+        }
+        else Console.WriteLine("\nНет данных, нужно создать список студентов");
+
+        StudentInfo.Message("\nДля выхода в меню нажмите *Enter* ");
+        Console.ReadKey();
+        Console.Clear();
+        Project.Menu(Students);
+    }
+}
