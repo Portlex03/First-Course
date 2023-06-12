c79d58a5a2309da1d6bc4b4708da37f627e8d55a
Лёша Портнягин <101197621+Portlex03@users.noreply.github.com>
2023-06-12 21:40:09 +0600

    Add files via upload

---
+++ Дискретная математика/Дейкстра/Algorithm.cs
@@ -0,0 +1,49 @@
+﻿namespace Univercity;
+
+internal class Дейкстра
+{
+    public static void Algorithm(int[,] matrix, int p1, int p2)
+    {
+        Console.Write($"Расстояние от вершины {p1} до вершины {p2}: ");
+        p1--; p2--;
+
+        // количество вершин
+        int n = matrix.GetLength(0);
+
+        // массив с минимумами столбцов 
+        int[] MinColums = new int[n];
+
+        // заполнение массива максимальными числами
+        for ( int i = 0; i < n; i++) MinColums[i] = int.MaxValue / 2;
+
+        // минимальный путь от вершины p1 до вершины p2
+        int len = 0;
+
+        // список использованных вершин
+        List<int> numbers = new() { p1 };
+
+        while (p1 != p2)
+        {
+            // копия строки массива
+            int[] line = new int[n];
+
+            // заполнение массива максимальными числами
+            for (int i = 0; i < n; i++) line[i] = int.MaxValue / 2;
+
+            // проходимся по строке массива с номером p1
+            for (int i = 0; i < n; i++)
+            {
+                if (!numbers.Contains(i))
+                {
+                    line[i] = Math.Min(matrix[p1, i] + len, MinColums[i]);
+
+                    MinColums[i] = Math.Min(line[i], MinColums[i]);
+                }
+            }
+            len = line.Min();
+            p1 = Array.IndexOf(line,len);
+            numbers.Add(p1);
+        }
+        Console.WriteLine($"Ответ {len}");
+    }
+}
---
+++ Дискретная математика/Дейкстра/Main_Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Univercity;
+
+namespace MyNameSpace;
+
+class Main_Program
+{
+    static void Main()
+    {
+        // считывание матрицы с файла
+        string[] fileMatrix = File.ReadAllLines("files/Матрица.txt");
+
+        int[,] matrix = new int[fileMatrix.Length,fileMatrix.Length];
+
+        for (int i = 0; i < fileMatrix.Length; i++)
+        {
+            string[] line = fileMatrix[i].Split('\t');
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (line[j] == "0") 
+                    matrix[i, j] = int.MaxValue / 2;
+                else 
+                    matrix[i,j] = int.Parse(line[j]);
+            }
+        }
+
+        Console.Write("Введите вершины, до которых нужно посчитать минимальный путь: ");
+        string[] s = Console.ReadLine().Split();
+
+        int m = int.Parse(s[0]);
+        int n = int.Parse(s[1]);
+
+        Дейкстра.Algorithm(matrix,m,n);
+    }
+}
